bcf639f348f374cff31557e2d622d71bd40bb244
Juergen Treml <mail@juergentreml.de>
2012-07-31 19:58:41 +0200

    Simplify code, add dbl click and esc key handler, improve style

--- leaflet.measure/leaflet.measure.js
+++ leaflet.measure/leaflet.measure.js
@@ -7,7 +7,7 @@ L.Control.Measure = L.Control.extend({
 		var className = 'leaflet-control-zoom',
 		    container = L.DomUtil.create('div', className);
 
-		this._createButton('Measure', 'leaflet-control-measure', container, this._toogleMeasure, this);
+		this._createButton('Measure', 'leaflet-control-measure', container, this._toggleMeasure, this);
 
 		return container;
 	},
@@ -26,7 +26,7 @@ L.Control.Measure = L.Control.extend({
 		return link;
 	},
 
-	_toogleMeasure: function () {
+	_toggleMeasure: function () {
 		this._measuring = !this._measuring;
 
 		if(this._measuring) {
@@ -42,8 +42,14 @@ L.Control.Measure = L.Control.extend({
 		this._oldCursor = this._map._container.style.cursor;
 		this._map._container.style.cursor = 'crosshair';
 
-		L.DomEvent.on(this._map, 'mousemove', this._mouseMove, this);
-		L.DomEvent.on(this._map, 'click', this._mouseClick, this);
+		this._doubleClickZoom = this._map.doubleClickZoom.enabled();
+		this._map.doubleClickZoom.disable();
+
+		L.DomEvent
+			.on(this._map, 'mousemove', this._mouseMove, this)
+			.on(this._map, 'click', this._mouseClick, this)
+			.on(this._map, 'dblclick', this._finishPath, this)
+			.on(document, 'keydown', this._onKeyDown, this);
 
 		if(!this._layerPaint) {
 			this._layerPaint = L.layerGroup().addTo(this._map);	
@@ -57,10 +63,19 @@ L.Control.Measure = L.Control.extend({
 	_stopMeasuring: function() {
 		this._map._container.style.cursor = this._oldCursor;
 
-		L.DomEvent.off(this._map, 'mousemove', this._mouseMove, this);
-		L.DomEvent.off(this._map, 'click', this._mouseClick, this);
+		L.DomEvent
+			.off(document, 'keydown', this._onKeyDown, this)
+			.off(this._map, 'mousemove', this._mouseMove, this)
+			.off(this._map, 'click', this._mouseClick, this)
+			.off(this._map, 'dblclick', this._mouseClick, this);
 
-		this._layerPaint.clearLayers();
+		if(this._doubleClickZoom) {
+			this._map.doubleClickZoom.enable();
+		}
+
+		if(this._layerPaint) {
+			this._layerPaint.clearLayers();
+		}
 		
 		this._restartPath();
 	},
@@ -73,8 +88,9 @@ L.Control.Measure = L.Control.extend({
 		if(!this._layerPaintPathTemp) {
 			this._layerPaintPathTemp = L.polyline([this._lastPoint, e.latlng], { 
 				color: 'black',
-				weight: 1,
-				clickable: false
+				weight: 1.5,
+				clickable: false,
+				dashArray: '6,3'
 			}).addTo(this._layerPaint);
 		} else {
 			this._layerPaintPathTemp.spliceLatLngs(0, 2, this._lastPoint, e.latlng);
@@ -93,25 +109,12 @@ L.Control.Measure = L.Control.extend({
 	},
 
 	_mouseClick: function(e) {
+		// Skip if no coordinates
 		if(!e.latlng) {
 			return;
 		}
 
-		if(this._lastCircle) {
-			this._layerPaint.removeLayer(this._lastCircle);
-		}
-
-		this._lastCircle = new L.CircleMarker(e.latlng, { 
-			color: 'red', 
-			opacity: 1, 
-			weight: 1, 
-			fill: true, 
-			fillOpacity: 1, 
-			clickable: this._lastCircle ? true : false
-		}).setRadius(3).addTo(this._layerPaint);
-
-		this._lastCircle.on('click', function() { this._finishPath(); }, this);
-
+		// If we have a tooltip, update the distance and create a new tooltip, leaving the old one exactly where it is (i.e. where the user has clicked)
 		if(this._lastPoint && this._tooltip) {
 			if(!this._distance) {
 				this._distance = 0;
@@ -124,30 +127,52 @@ L.Control.Measure = L.Control.extend({
 		}
 		this._createTooltip(e.latlng);
 
-		if(!this._lastPoint) {
-			this._lastPoint = e.latlng;
-			return;
-		}
-
-		if(!this._layerPaintPath) {
-			this._layerPaintPath = L.polyline([this._lastPoint, e.latlng], { 
-				color: 'red',
+		// If this is already the second click, add the location to the fix path (create one first if we don't have one)
+		if(this._lastPoint && !this._layerPaintPath) {
+			this._layerPaintPath = L.polyline([this._lastPoint], { 
+				color: 'black',
+				weight: 2,
 				clickable: false
 			}).addTo(this._layerPaint);
-			
-			this._lastPoint = e.latlng;
-
-			return;
 		}
 
+		if(this._layerPaintPath) {
+			this._layerPaintPath.addLatLng(e.latlng);
+		}
+
+		// Upate the end marker to the current location
+		if(this._lastCircle) {
+			this._layerPaint.removeLayer(this._lastCircle);
+		}
+
+		this._lastCircle = new L.CircleMarker(e.latlng, { 
+			color: 'black', 
+			opacity: 1, 
+			weight: 1, 
+			fill: true, 
+			fillOpacity: 1, 
+			clickable: this._lastCircle ? true : false
+		}).setRadius(2).addTo(this._layerPaint);
+
+		this._lastCircle.on('click', function() { this._finishPath(); }, this);
+
+		// Save current location as last location
 		this._lastPoint = e.latlng;
-		this._layerPaintPath.addLatLng(e.latlng);
-		this._createTooltip(e.latlng);
 	},
 
 	_finishPath: function() {
-		this._layerPaint.removeLayer(this._lastCircle);
-		this._layerPaint.removeLayer(this._tooltip);
+		// Remove the last end marker as well as the last (moving tooltip)
+		if(this._lastCircle) {
+			this._layerPaint.removeLayer(this._lastCircle);
+		}
+		if(this._tooltip) {
+			this._layerPaint.removeLayer(this._tooltip);
+		}
+		if(this._layerPaint && this._layerPaintPathTemp) {
+			this._layerPaint.removeLayer(this._layerPaintPathTemp);
+		}
+
+		// Reset everything
 		this._restartPath();
 	},
 
@@ -176,9 +201,12 @@ L.Control.Measure = L.Control.extend({
 	},
 
 	_updateTooltipDistance: function(total, difference) {
-		var text = '<div class="leaflet-measure-tooltip-total">' + this._round(total) + ' nm</div>';
-		if(total != difference) {
-			text += '<div class="leaflet-measure-tooltip-difference">(+' + this._round(difference) + ' nm)</div>';
+		var totalRound = this._round(total),
+			differenceRound = this._round(difference);
+
+		var text = '<div class="leaflet-measure-tooltip-total">' + totalRound + ' nm</div>';
+		if(totalRound != differenceRound) {
+			text += '<div class="leaflet-measure-tooltip-difference">(+' + differenceRound + ' nm)</div>';
 		}
 
 		this._tooltip._icon.innerHTML = text;
@@ -186,6 +214,17 @@ L.Control.Measure = L.Control.extend({
 
 	_round: function(val) {
 		return Math.round((val / 1852) * 10) / 10;
+	},
+
+	_onKeyDown: function (e) {
+		if(e.keyCode == 27) {
+			// If not in path exit measuring mode, else just finish path
+			if(!this._lastPoint) {
+				this._toggleMeasure();
+			} else {
+				this._finishPath();
+			}
+		}
 	}
 });
 
